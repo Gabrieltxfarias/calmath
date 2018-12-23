bc7c3d7221e50616b2001c746d01e81d3b16ef54
Gabrieltxfarias <39681861+Gabrieltxfarias@users.noreply.github.com>
2018-12-23 16:34:53 -0300

    Add files via upload

--- calculabhaskara.js
+++ calculabhaskara.js
@@ -11,6 +11,7 @@ function CalculaBhask(){
     var numeroB = parseInt(document.getElementById('numeroB').value);
     var numeroC = parseInt(document.getElementById('numeroC').value);
 
+
     this.telaresult.value = Math.pow(numeroB, 2) - 4*numeroA*numeroC;
     this.telaresult2.value = ((numeroB*(-1)) + Math.sqrt(Math.pow(numeroB, 2) - 4*numeroA*numeroC))/ numeroA*2;
     this.telaresult3.value = ((numeroB*-1)- Math.sqrt(Math.pow(numeroB, 2) - 4*numeroA*numeroC))/ 2*numeroA;
